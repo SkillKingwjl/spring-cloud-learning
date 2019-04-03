5957295fbc8da0cb13753f56316429d83fdb2c4f
魏江龙 <383188327@qq.com>
2019-04-03 16:13:19 +0800

    config-client配置

---
+++ config-client/src/main/java/com/wjl/configclient/ConfigClientApplication.java
@@ -0,0 +1,23 @@
+package com.wjl.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+    @Value("${version}")
+    String version;
+
+    @RequestMapping(value = "/version")
+    public String version(){
+        return version;
+    }
+}
