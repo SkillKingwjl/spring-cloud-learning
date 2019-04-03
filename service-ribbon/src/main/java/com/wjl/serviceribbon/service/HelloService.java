24d7f441d85ee6fd9b54b28c1430724ecb8448a7
魏江龙 <383188327@qq.com>
2019-04-03 14:38:57 +0800

    ribbon使用Hystrix熔断机制

--- service-ribbon/src/main/java/com/wjl/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/wjl/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.wjl.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,12 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "helloErr")
     public String hello(String name){
         return restTemplate.getForObject("http://SERVER-HI/hi?name="+name,String.class);
+    }
+
+    public String helloErr(String name){
+        return "调用服务出错";
     }
 }
